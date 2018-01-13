03859a6e8ead93efa4e59d74db6a4cf7230543ac
BBQCHICKENS <gqq654@163.com>
2018-01-13 23:43:25 +0800

    测试mybatis的新添加的文件上传

--- ecps-console/src/main/java/com/lbj/ecps/controller/UserController.java
+++ ecps-console/src/main/java/com/lbj/ecps/controller/UserController.java
@@ -1,9 +1,27 @@
 package com.lbj.ecps.controller;
 
+import com.lbj.ecps.model.Student;
+import com.lbj.ecps.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by gqq on 2018/1/13.
  */
+@Controller
+@RequestMapping(value = "/student")
 public class UserController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @RequestMapping(value = "getStudent.do",method = RequestMethod.GET)
+    public void getStudent(Integer id){
+        Student student = this.studentService.selectByPrimaryKey(id);
+        System.out.println(student);
+    }
 
 
 }
